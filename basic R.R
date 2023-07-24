03cc47513d16ad06cb42b154f86d57d5287ee736
shreyasbarge007 <140387695+shreyasbarge007@users.noreply.github.com>
2023-07-24 14:33:07 +0530

    Add files via upload

---
+++ basic R.R
@@ -0,0 +1,33 @@
+a <- 100
+class(a)
+
+bool <- FALSE
+class(bool)
+
+name <- "Python"
+class(name)
+
+x <- 10+10
+print(x)
+typeof(x)
+class(x)
+
+x <- 10
+y <- 2
+z <- x*y
+
+m <- c(12,22,33,44)
+
+#vector
+info <- c(1,"Shreyas",88,1.2,TRUE)
+info[0:2]
+info[1:2]
+info[3:4]
+info[5]
+info[0:5]
+match('88',info)
+info[2]
+info[3]
+info[3] <- "TRUE"
+info[4]
+info[4] <- 2.2
